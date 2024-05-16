0ae686b9cb7f48f62d508d67c8d3ceea8dbf5109
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-05-16 19:22:01 +0700

    chore: update api teacher
    add auth

--- app/api/teachers/route.ts
+++ app/api/teachers/route.ts
@@ -1,11 +1,20 @@
 import {NextResponse} from "next/server";
 import {db} from "@/lib/db";
+import {authHandler} from "@/lib/helper";
+import {UserRole} from "@prisma/client";
 
 export async function GET(req: Request) {
     console.log("Timestamp: ", new Date().toLocaleString());
     console.log("GET ", req.url);
+
     try {
-        const teachers = await db.teacher.findMany();
+        await authHandler([UserRole.admin, UserRole.teacher]);
+    } catch (error) {
+        return new NextResponse(JSON.stringify(error), {status: 401});
+    }
+
+    try {
+        const teachers = await db.teacher.findMany({include: {user: true}});
         return new NextResponse(JSON.stringify(teachers), {status: 200});
     } catch (error) {
         console.log("Error: ", error);
