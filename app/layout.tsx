743890c8eba7e126adcf073adce3a523ff64199c
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-05-18 11:51:38 +0700

    feat: change radix to shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,16 @@
 import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import {Inter as FontSans} from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
 
-const inter = Inter({subsets: ["latin"]});
+import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/components/theme-provider";
+import {Toaster} from "@/components/ui/toaster";
+
+const fontSans = FontSans({
+    subsets: ["latin"],
+    variable: "--font-sans",
+});
 
 export const metadata: Metadata = {
     title: "Create Next App",
@@ -17,20 +24,23 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>
-                {/* <ThemeProvider attribute="class">
-                    <Theme
-                        accentColor="mint"
-                        hasBackground
-                        panelBackground="solid"
-                        scaling="100%"
-                        radius="full"
-                        grayColor="gray"
-                    >
-                        <ClerkProvider>{children}</ClerkProvider>
-                    </Theme>
-                </ThemeProvider> */}
-                <ClerkProvider>{children}</ClerkProvider>
+            <body
+                className={cn(
+                    "min-h-screen bg-background font-sans antialiased dark:bg-black",
+                    fontSans.variable
+                )}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    <ClerkProvider>
+                        <main>{children}</main>
+                        <Toaster />
+                    </ClerkProvider>
+                </ThemeProvider>
             </body>
         </html>
     );
