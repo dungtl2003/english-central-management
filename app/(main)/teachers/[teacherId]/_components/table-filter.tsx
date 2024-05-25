0455c6f5ab1878a2dd5c7d94056464f0c5bed97c
PinkRex <ducnguyenminh1912003@gmail.com>
2024-05-25 16:19:28 +0700

    chore: fix table filter

--- app/(main)/teachers/[teacherId]/_components/table-filter.tsx
+++ app/(main)/teachers/[teacherId]/_components/table-filter.tsx
@@ -24,6 +24,7 @@ const TableFilter = (props: TableFilterProps): ReactElement => {
         _setFilterType(key);
         _setSelectedRadio(key);
         props.table.setSorting([]);
+        props.table.setColumnFilters([]);
         props.table.setRowSelection({});
     };
 
