d97458c1013fdf5ccb1a2724046aaff2f5815c24
PinkRex <ducnguyenminh1912003@gmail.com>
2024-05-26 15:07:12 +0700

    chore: add class detail overview

--- app/(main)/teachers/[teacherId]/classes/[classId]/_components/class-header.tsx
+++ app/(main)/teachers/[teacherId]/classes/[classId]/_components/class-header.tsx
@@ -3,11 +3,11 @@ import {FaPlusCircle} from "react-icons/fa";
 import React, {ReactElement} from "react";
 import {DatePicker} from "./date-picker";
 
-const ClassHeader = (): ReactElement => {
+const ClassDetailHeader = (): ReactElement => {
     return (
         <div className="flex flex-row items-center">
             <span className="text-5xl font-semibold">Class Detail</span>
-            <div className="flex flex-row items-center ml-auto  gap-x-2">
+            <div className="flex flex-row items-center ml-auto gap-x-2">
                 <DatePicker />
                 <Button variant="outline">
                     <span className="pr-1">
@@ -20,4 +20,4 @@ const ClassHeader = (): ReactElement => {
     );
 };
 
-export default ClassHeader;
+export default ClassDetailHeader;
--- app/(main)/teachers/[teacherId]/classes/[classId]/page.tsx
+++ app/(main)/teachers/[teacherId]/classes/[classId]/page.tsx
@@ -1,14 +1,19 @@
-import {Card, CardContent} from "@/components/ui/card";
+"use client";
+
+import {Card, CardContent, CardHeader} from "@/components/ui/card";
 import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
 import React, {ReactElement} from "react";
-import ClassHeader from "./_components/class-header";
+import ClassDetailHeader from "./_components/class-header";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import OverviewCard from "./_components/overview-card";
 
 const page = (): ReactElement => {
     return (
         <div className="flex justify-center">
-            <div className="w-10/12 pt-[110px]">
-                <ClassHeader />
-                <Tabs defaultValue="overview" className="mt-[30px] w-full">
+            <div className="w-[80%] pt-[100px]">
+                <ClassDetailHeader />
+                <Tabs defaultValue="overview" className="mt-[15px] w-full">
                     <TabsList>
                         <TabsTrigger value="overview">Overview</TabsTrigger>
                         <TabsTrigger value="classList">Class list</TabsTrigger>
@@ -17,16 +22,79 @@ const page = (): ReactElement => {
                         </TabsTrigger>
                     </TabsList>
                     <TabsContent value="overview">
-                        <Card className="font-mono flex align-middle">
-                            <CardContent className="space-y-2 align-middle">
-                                haha
-                            </CardContent>
-                        </Card>
-                        <Card className="font-mono">
-                            <CardContent className="space-y-2">
-                                hihi
-                            </CardContent>
-                        </Card>
+                        <OverviewCard />
+                        <div className="pt-5 grid grid-cols-5 gap-x-5">
+                            <div className="col-span-2">
+                                <Card className="">
+                                    <CardHeader className="text-2xl font-semibold">
+                                        Class information
+                                    </CardHeader>
+                                    <CardContent className="">
+                                        <div className="pb-3 grid grid-cols-4 items-center gap-x-3">
+                                            <Label
+                                                htmlFor="className"
+                                                className="text-left"
+                                            >
+                                                Class name
+                                            </Label>
+                                            <Input
+                                                id="className"
+                                                value="3.1"
+                                                className="col-span-3"
+                                                disabled
+                                            />
+                                        </div>
+                                        <div className="pb-3 grid grid-cols-4 items-center gap-x-3">
+                                            <Label
+                                                htmlFor="grade"
+                                                className="text-left"
+                                            >
+                                                Grade
+                                            </Label>
+                                            <Input
+                                                id="grade"
+                                                value="3"
+                                                className="col-span-3"
+                                                disabled
+                                            />
+                                        </div>
+                                        <div className="pb-3 grid grid-cols-4 items-center gap-x-3">
+                                            <Label
+                                                htmlFor="startDate"
+                                                className="text-left"
+                                            >
+                                                Start date
+                                            </Label>
+                                            <Input
+                                                id="startDate"
+                                                value="01/01/2024"
+                                                className="col-span-3"
+                                                disabled
+                                            />
+                                        </div>
+                                        <div className="pb-3 grid grid-cols-4 items-center gap-x-3">
+                                            <Label
+                                                htmlFor="endDate"
+                                                className="text-left"
+                                            >
+                                                End date
+                                            </Label>
+                                            <Input
+                                                id="endDate"
+                                                value="01/02/2024"
+                                                className="col-span-3"
+                                                disabled
+                                            />
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                            <div className="col-span-3">
+                                <Card className="">
+                                    <CardContent className=""></CardContent>
+                                </Card>
+                            </div>
+                        </div>
                     </TabsContent>
                     <TabsContent value="classList">
                         This is class detail list
