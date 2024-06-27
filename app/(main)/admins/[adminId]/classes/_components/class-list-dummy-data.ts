79c2f381a903732cd0d365f3a2f08279292e4e35
PinkRex <ducnguyenminh1912003@gmail.com>
2024-06-28 01:41:30 +0700

    chore: add more teacher info in create new class

--- app/(main)/admins/[adminId]/classes/_components/class-list-dummy-data.ts
+++ app/(main)/admins/[adminId]/classes/_components/class-list-dummy-data.ts
@@ -1,4 +1,4 @@
-import {ClassListModel, UnitModel} from "./types";
+import {ClassListModel, UnitModel, TeacherModel} from "./types";
 
 export const classListDummyData: ClassListModel[] = [
     {
@@ -415,19 +415,95 @@ export const unitDummyData: UnitModel[] = [
     },
 ];
 
-export const teacherDummyData: string[] = [
-    "Nguyễn Minh Đức",
-    "Trần Lưu Dũng",
-    "Nguyễn Hữu Nhật Quang",
-    "Nguyễn Gia Huy",
-    "Nguyễn Quang Minh",
-    "Nguyễn Thảo Chi",
-    "Trịnh Văn Bình",
-    "Bùi Minh Khánh",
-    "Khương Quốc Vượng",
-    "Trần Hồng Vân",
-    "Nguyễn Thị Thảo",
-    "Mai Thanh Nga",
-    "Nguyễn Hoàng Dũng",
-    "Trần Chí Nhật",
+export const teacherDummyData: TeacherModel[] = [
+    {
+        fullName: "Nguyễn Minh Đức",
+        id: "123456789123456789123456879a",
+        birthday: "01/01/2003",
+        createDate: "01/01/2022",
+    },
+    {
+        fullName: "Trần Lưu Dũng",
+        id: "123456789123456789123456879b",
+        birthday: "02/01/2003",
+        createDate: "02/01/2022",
+    },
+    {
+        fullName: "Trần Lưu Dũng",
+        id: "123456789123456789123456879o",
+        birthday: "02/01/2003",
+        createDate: "02/01/2022",
+    },
+    {
+        fullName: "Nguyễn Hữu Nhật Quang",
+        id: "123456789123456789123456879c",
+        birthday: "03/01/2003",
+        createDate: "03/01/2022",
+    },
+    {
+        fullName: "Nguyễn Gia Huy",
+        id: "123456789123456789123456879d",
+        birthday: "04/01/2003",
+        createDate: "04/01/2022",
+    },
+    {
+        fullName: "Nguyễn Quang Minh",
+        id: "123456789123456789123456879e",
+        birthday: "05/01/2003",
+        createDate: "05/01/2022",
+    },
+    {
+        fullName: "Nguyễn Thảo Chi",
+        id: "123456789123456789123456879f",
+        birthday: "06/01/2003",
+        createDate: "06/01/2022",
+    },
+    {
+        fullName: "Trịnh Văn Bình",
+        id: "123456789123456789123456879g",
+        birthday: "07/01/2003",
+        createDate: "07/01/2022",
+    },
+    {
+        fullName: "Bùi Minh Khánh",
+        id: "123456789123456789123456879h",
+        birthday: "08/01/2003",
+        createDate: "08/01/2022",
+    },
+    {
+        fullName: "Khương Quốc Vượng",
+        id: "123456789123456789123456879i",
+        birthday: "09/01/2003",
+        createDate: "09/01/2022",
+    },
+    {
+        fullName: "Nguyễn Thị Thảo",
+        id: "123456789123456789123456879j",
+        birthday: "10/01/2003",
+        createDate: "10/01/2022",
+    },
+    {
+        fullName: "Trần Hồng Vân",
+        id: "123456789123456789123456879k",
+        birthday: "11/01/2003",
+        createDate: "11/01/2022",
+    },
+    {
+        fullName: "Mai Thanh Nga",
+        id: "123456789123456789123456879l",
+        birthday: "12/01/2003",
+        createDate: "12/01/2022",
+    },
+    {
+        fullName: "Nguyễn Hoàng Dũng",
+        id: "123456789123456789123456879m",
+        birthday: "13/01/2003",
+        createDate: "13/01/2022",
+    },
+    {
+        fullName: "Trần Chí Nhật",
+        id: "123456789123456789123456879n",
+        birthday: "13/01/2003",
+        createDate: "13/01/2022",
+    },
 ];
