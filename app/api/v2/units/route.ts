3b7523eafdb356c751a7627de80ae2224d6f4180
Tran Luu Dung <63690773+dungtl2003@users.noreply.github.com>
2024-07-01 19:02:47 +0700

    feat: add unit api

--- app/api/v2/units/route.ts
+++ app/api/v2/units/route.ts
@@ -6,8 +6,56 @@ import {auth} from "@clerk/nextjs/server";
 import {Prisma, UserRole} from "@prisma/client";
 import {ApiError} from "next/dist/server/api-utils";
 import {NextRequest, NextResponse} from "next/server";
-import {PostRequestPayload, PostResponsePayload} from "./types";
+import {
+    GetResponsePayload,
+    PostRequestPayload,
+    PostResponsePayload,
+} from "./types";
 import {PostRequestPayloadSchema} from "./schema";
+import {ErrorResponsePayload} from "@/constaints";
+
+/**
+ * Get units.
+ * Only admin can access this api.
+ */
+export async function GET(
+    req: NextRequest
+): Promise<NextResponse<GetResponsePayload | ErrorResponsePayload>> {
+    console.log("Timestamp: ", new Date().toLocaleString());
+    console.log("GET ", req.nextUrl.pathname);
+
+    try {
+        const clerkUserId = auth().userId;
+        const role: UserRole | null = getClerkRole();
+
+        if (!clerkUserId) {
+            throw new ApiError(401, "No signed in user");
+        }
+
+        if (!role || role !== UserRole.ADMIN) {
+            throw new ApiError(401, "No right permission");
+        }
+
+        const admin = await db.user.findFirst({
+            where: {
+                referId: clerkUserId,
+                role: UserRole.ADMIN,
+            },
+        });
+
+        if (!admin) {
+            throw new ApiError(401, `Account not found`);
+        }
+
+        const units = await db.unit.findMany({});
+
+        return NextResponse.json<GetResponsePayload>(units, {
+            status: 200,
+        });
+    } catch (error) {
+        return buildErrorNextResponse(error);
+    }
+}
 
 /**
  * Add unit.
